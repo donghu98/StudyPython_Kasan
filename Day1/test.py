90716943b22182d826b32568f5db6f1e441291d4
김동후 <tomlmot@naver.com>
2022-05-23 17:54:56 +0900

    22.05.23 최종커밋

---
+++ Day1/test.py
@@ -0,0 +1,33 @@
+from re import A
+from tkinter import Variable
+
+
+print('hello python!') 
+
+a='hello python.'
+print(a)
+
+a = 10
+print(a)
+
+a = False
+print(a)
+
+# 주석입니다. 주석은 소스 실행에 아무런 영향을 미치지않습니다.
+
+#변수명 지정 영문자 _로 시작 특수문자 불가 
+Varl = 17
+print(Varl)
+# 4varl = 4 
+# 5_var = 5
+var_5 = 5
+ #숫자로 시작할 수 없음
+ #특수문자 _ 이외에는 사용불가 
+var_8_ = 8.8
+# var 10 = 10 #중간에 공백도 불가 
+
+var1 = 17
+Var1 = 19
+#대소문자 *반드시* 구분
+
+print(var1 == Var1) #문법적 표현 -> ==
